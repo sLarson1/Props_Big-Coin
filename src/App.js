7599630d1dc007e792830739c7236fe79c8ac793
sLarson1 <larson.sean@gmail.com>
2024-05-01 10:19:55 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import BigCoin from './BigCoin';
+
+function App() {
+
+  const [numOfCoins, setNumOfCoins] = React.useState(0);  
+
+  return (
+    <div className="wrapper">
+      <main>
+        <BigCoin numOfCoins={numOfCoins} setNumOfCoins={setNumOfCoins} />
+      </main>
+      <footer>
+        Your coin balance:
+        <strong>{numOfCoins}</strong>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
